6d14ddcbbb2a39656af73090f18b828d4d2c8bdd
Ada Prywata <adacien@o2.pl>
2023-10-08 17:03:28 +0200

    typo

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export const revlidate = 10;
+export const revalidate = 10;
 
 export async function GET(request: NextRequest): Promise<Response> {
 	return NextResponse.json(request.headers);
