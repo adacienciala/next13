8d2fe52d57de0ce72f5cfd1294d6d94028c945ec
Ada Prywata <adacien@o2.pl>
2023-10-10 20:43:56 +0200

    rewrites->redirects

--- next.config.js
+++ next.config.js
@@ -16,15 +16,17 @@ const nextConfig = {
 			},
 		],
 	},
-	async rewrites() {
+	async redirects() {
 		return [
 			{
 				source: "/products",
 				destination: "/products/1",
+				permanent: true,
 			},
 			{
 				source: "/categories/:slug",
 				destination: "/categories/:slug/1",
+				permanent: true,
 			},
 		];
 	},
